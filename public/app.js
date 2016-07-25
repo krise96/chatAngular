6a00690e633888ca8950285ed848345fbf332631
Mykola <krise96@gmail.com>
2016-07-25 19:28:07 +0300

    fix bags

--- public/app.js
+++ public/app.js
@@ -1,4 +1,14 @@
 (function() {
-        angular
-                .module('app', []);
+        var module = angular.module('app', ['ngRoute']);
+
+
+	module
+		.config(['$routeProvider', function($routeProvider) {
+			$routeProvider
+				.when('/', {
+					templateUrl: './index.html',
+				})
+		}]);
 })();
+
+
